f989a9dccfa3619cc71e5aaa4e567629f5cdf92f
Fernando Felicissimo <ffelicissimo@gmail.com>
2016-04-22 10:41:54 -0300

    Create Mysql Rancher

--- mysql/startup.sh
+++ mysql/startup.sh
@@ -4,8 +4,7 @@ f [ ! -f /var/lib/mysql/rancher]; then
 
         /usr/bin/mysqld_safe &
         sleep 10s
-        echo "CREATE DATABASE rancher COLLATE = 'utf8_general_ci' CHARACTER SET = 'utf8';" | mysql
-        echo "GRANT ALL PRIVILEGES ON *.* TO 'rancher'@'localhost' IDENTIFIED BY 'rancher' WITH GRANT OPTION;" | mysql
+        echo "CREATE DATABASE rancher COLLATE = 'utf8_general_ci' CHARACTER SET = 'utf8'; GRANT ALL PRIVILEGES ON *.* TO 'rancher'@'localhost' IDENTIFIED BY 'rancher' WITH GRANT OPTION;" | mysql
         killall mysqld
         sleep 10s
 fi
